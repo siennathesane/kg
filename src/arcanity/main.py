34302c5de868289448961d1306f753649102f8aa
Sienna Lloyd <sienna@linux.com>
2023-03-22 16:23:30 -0700

    new endpoint and some comments
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- src/arcanity/main.py
+++ src/arcanity/main.py
@@ -8,23 +8,51 @@ from sanic.response import JSONResponse, json
 from sanic.worker.manager import WorkerManager
 from spacy.tokens.doc import Doc
 
+# hide the warning logs
 simplefilter("ignore")
+
+# set up the app and set the worker boot timer
 app = Sanic("arcanity")
 WorkerManager.THRESHOLD = 1800
 
 
 @app.before_server_start
 async def nlp_loader(app_ctx, _):
+    """
+    Load the NLP model before the server starts. Currently, the only model is `en_core_web_trf`
+    Args:
+        app_ctx: Sanic Application object
+        _:
+
+    Returns: None
+
+    """
     app_ctx.ctx.nlp = spacy.load("en_core_web_trf")
 
 
 @app.on_request
-async def load_model(request):
+async def load_model(request: Request):
+    """
+    Make a reference to the model per-request to ensure processing individual requests is faster
+    Args:
+        request: HTTP request object
+
+    Returns:
+
+    """
     request.ctx.nlp = app.ctx.nlp
 
 
 @app.post("/v1/extract")
 async def extract(request: Request) -> JSONResponse:
+    """
+    Extract the information from free-form text.
+    Args:
+        request: An HTTP POST request.
+
+    Returns: JSON HTTP response
+
+    """
     # noinspection PyUnusedLocal
     parsed: Doc = None
     body = request.json
@@ -38,6 +66,21 @@ async def extract(request: Request) -> JSONResponse:
     d = parsed.to_json()
     del d["text"]
     return json(d)
+
+
+@app.get("/v1/features")
+async def load_labels(request: Request) -> JSONResponse:
+    """
+    Determine the available features for the NLP Service
+    Args:
+        request: HTTP request object
+
+    Returns: JSON HTTP response
+
+    """
+    entities = request.ctx.nlp.get_pipe("ner").labels
+    tags = request.ctx.nlp.get_pipe("tagger").labels
+    return json({"entities": entities, "partsOfSpeech": tags})
 
 
 if __name__ == '__main__':
